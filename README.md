42b9c0aca649bbad70f408ee969eb957732ba7aa
RyanParsley <rmparsley@gmail.com>
2021-06-09 09:45:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-panel-demo-aj9uyq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-panel-demo-aj9uyq)
---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+import {MessageService} from 'primeng/api';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  providers: [MessageService]
+})
+export class AppComponent { 
+    items: MenuItem[];
+    
+    constructor(private messageService: MessageService) {}
+    
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Options',
+                items: [{
+                    label: 'Update',
+                    icon: 'pi pi-refresh',
+                    command: () => {
+                        this.update();
+                    }
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-times',
+                    command: () => {
+                        this.delete();
+                    }
+                }
+            ]},
+            {
+                label: 'Navigate',
+                items: [{
+                    label: 'Angular Website',
+                    icon: 'pi pi-external-link',
+                    url: 'http://angular.io'
+                },
+                {
+                    label: 'Router',
+                    icon: 'pi pi-upload',
+                    routerLink: '/fileupload'
+                }
+            ]}
+        ];
+    }
+
+    update() {
+        this.messageService.add({severity:'success', summary:'Success', detail:'Data Updated'});
+    }
+
+    delete() {
+        this.messageService.add({severity:'warn', summary:'Delete', detail:'Data Deleted'});
+    }
+}
